03ebd19184b369a14af56c4c3304aceb04b5d83d
Jacek Piszczek <jacek.piszczek@runbox.com>
2020-10-21 19:06:16 +0200

    Fixed right shift handling. Enabled intersection observer.

--- Source/WebKitLegacy/morphos/WebProcess.cpp
+++ Source/WebKitLegacy/morphos/WebProcess.cpp
@@ -141,6 +141,8 @@ void WebProcess::initialize(int sigbit)
 	RuntimeEnabledFeatures::sharedFeatures().setDataTransferItemsEnabled(true);
 	RuntimeEnabledFeatures::sharedFeatures().setAccessibilityObjectModelEnabled(false);
 
+	RuntimeEnabledFeatures::sharedFeatures().setIntersectionObserverEnabled(true);
+
 	m_dummyNetworkingContext = WebFrameNetworkingContext::create(nullptr);
 
 	WTF::FileSystemImpl::makeAllDirectories("PROGDIR:Cache/FavIcons");
