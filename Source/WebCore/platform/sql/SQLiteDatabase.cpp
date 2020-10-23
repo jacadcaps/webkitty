164aed4461abc38285a6c504940e8cfa57b38c4b
Jacek Piszczek <jacek.piszczek@runbox.com>
2020-10-23 17:35:22 +0200

    Disabled synchronous writes in SQL

--- Source/WebCore/platform/sql/SQLiteDatabase.cpp
+++ Source/WebCore/platform/sql/SQLiteDatabase.cpp
@@ -162,7 +162,10 @@ bool SQLiteDatabase::open(const String& filename, OpenMode openMode)
 
 void SQLiteDatabase::useWALJournalMode()
 {
-#if !OS(MORPHOS)
+#if OS(MORPHOS)
+	SQLiteStatement syncStatement(*this, "PRAGMA synchronous=off;"_s);
+	syncStatement.prepareAndStep();
+#else
     m_useWAL = true;
     {
         SQLiteStatement walStatement(*this, "PRAGMA journal_mode=WAL;"_s);
