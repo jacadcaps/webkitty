aa41fd64c8f9dc81ede678ff200a3c4915eaefcf
Jacek Piszczek <jacek.piszczek@runbox.com>
2021-05-28 16:14:51 +0200

    Fixed ImageBufferBackend pixel conversion issues

--- Source/WebCore/platform/graphics/ImageBufferBackend.h
+++ Source/WebCore/platform/graphics/ImageBufferBackend.h
@@ -58,6 +58,9 @@ enum class PreserveResolution : uint8_t {
 enum class PixelFormat : uint8_t {
     RGBA8,
     BGRA8,
+#if CPU(BIG_ENDIAN)
+    ARGB8 = BGRA8, // BGRA will actually be ARGB on BIG_ENDIAN
+#endif
     RGB10,
     RGB10A8,
 };
